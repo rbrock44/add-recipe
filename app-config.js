c881073913733a857f160616aeddaf556bbd2673
rbrock44 <rbrock44@users.noreply.github.com>
2026-04-24 20:57:07 +0000

    deploy: 05cfebaa04cd1a2ffac18b90ac210ab255e94fb3

---
+++ app-config.js
@@ -0,0 +1,4 @@
+window.__APP_CONFIG__ = {
+  "pendingRecipeUrl": "https://family-recipes.ryan-brock.com/recipes/pending",
+  "deleteRecipeToken": "vDGKEVPH87BDfzbIswIwLHOULDpd5lTpXuu0wjEmxM2uIQuLmzAYp3fJcqeibIY5"
+};
