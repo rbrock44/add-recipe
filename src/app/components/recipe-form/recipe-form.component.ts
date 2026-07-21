93aa02aac5d63362185a6d406720de6ff9e022eb
rbrock44 <rbrock444@gmail.com>
2026-07-20 22:05:27 -0500

    upgrade angular to 21

--- src/app/components/recipe-form/recipe-form.component.ts
+++ src/app/components/recipe-form/recipe-form.component.ts
@@ -10,7 +10,7 @@ import {MatSelectModule} from '@angular/material/select';
 import {MatInputModule} from '@angular/material/input';
 import {MatCardModule} from '@angular/material/card';
 import {MatFormFieldModule} from '@angular/material/form-field';
-import {CommonModule} from '@angular/common';
+
 import {HttpClient} from '@angular/common/http';
 
 @Component({
@@ -19,7 +19,6 @@ import {HttpClient} from '@angular/common/http';
   templateUrl: './recipe-form.component.html',
   styleUrls: ['./recipe-form.component.scss'],
   imports: [
-    CommonModule,
     ReactiveFormsModule,
     MatCardModule,
     MatFormFieldModule,
@@ -29,7 +28,7 @@ import {HttpClient} from '@angular/common/http';
     MatIconModule,
     MatDividerModule,
     MatSnackBarModule
-  ]
+]
 })
 export class RecipeFormComponent implements OnInit {
   recipeForm: FormGroup = new FormGroup('');
